bd2bee32682c8d457b3d085e277e0c44cc227ebb
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-18 15:24:55 +0100

    fizzBuzz excercise

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const isLandscape = (width, height) => {
-  return (width > height);
+// divisable by 3 => Fizz
+// divisisable by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not divisible by 3 -r 5 => number
+// Not a number => 'Not a number'
+
+const fizzBuzz = (num) => {
+  if (typeof num !== 'number') return NaN;
+  if (num % 15 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz'; 
+  return num;
 }
 
-console.log(isLandscape(10, 10));
+const output = fizzBuzz(13);
+console.log(output);
