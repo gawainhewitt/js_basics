089fc852a4c469dcd3c856c1556257ec4d442480
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-18 15:55:37 +0100

    checkspeed exercise

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// divisable by 3 => Fizz
-// divisisable by 5 => Buzz
-// Divisible by both 3 and 5 => FizzBuzz
-// Not divisible by 3 -r 5 => number
-// Not a number => 'Not a number'
+// every five more than 70 -> 1 Point
+// Math.floor(1.3)
+// 12 points -> suspended
 
-const fizzBuzz = (num) => {
-  if (typeof num !== 'number') return NaN;
-  if (num % 15 === 0) return 'FizzBuzz';
-  if (num % 3 === 0) return 'Fizz';
-  if (num % 5 === 0) return 'Buzz'; 
-  return num;
+const checkspeed = (speed) => {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  if (speed > speedLimit) {
+    const points = Math.floor((speed-speedLimit)/kmPerPoint);
+    if (points >= 1) return (points >= 12) ? 'Suspended': `Points: ${points}`;
+  }
+  return 'Ok';
 }
 
-const output = fizzBuzz(13);
-console.log(output);
+console.log(checkspeed(71));
+
