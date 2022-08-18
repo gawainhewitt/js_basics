5ec92eaf9257af7fd2f5bb930212e34e3c6cef19
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-18 15:56:52 +0100

    checkspeed exercise

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// // divisable by 3 => Fizz
+// // divisisable by 5 => Buzz
+// // Divisible by both 3 and 5 => FizzBuzz
+// // Not divisible by 3 -r 5 => number
+// // Not a number => 'Not a number'
+
+// const fizzBuzz = (num) => {
+//   if (typeof num !== 'number') return NaN;
+//   if (num % 15 === 0) return 'FizzBuzz';
+//   if (num % 3 === 0) return 'Fizz';
+//   if (num % 5 === 0) return 'Buzz'; 
+//   return num;
+// }
+
+// const output = fizzBuzz(13);
+// console.log(output);
+
 // every five more than 70 -> 1 Point
 // Math.floor(1.3)
 // 12 points -> suspended
