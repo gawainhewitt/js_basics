f9f11e03216fae20d18c5fa8f59e5bc33f5b90c7
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-18 16:07:38 +0100

    showNumbers exercise

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// showNumbers
+
+const showNumbers = (limit) => {
+  for (let i = 0; i <= limit; i++){
+    console.log(`${i} ${(i % 2 === 0) ? '"EVEN"' : '"ODD"'}`);
+  }
+}
+
+showNumbers(3);
+
+
+
 // // divisable by 3 => Fizz
 // // divisisable by 5 => Buzz
 // // Divisible by both 3 and 5 => FizzBuzz
@@ -19,15 +31,15 @@
 // Math.floor(1.3)
 // 12 points -> suspended
 
-const checkspeed = (speed) => {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  if (speed > speedLimit) {
-    const points = Math.floor((speed-speedLimit)/kmPerPoint);
-    if (points >= 1) return (points >= 12) ? 'Suspended': `Points: ${points}`;
-  }
-  return 'Ok';
-}
+// const checkspeed = (speed) => {
+//   const speedLimit = 70;
+//   const kmPerPoint = 5;
+//   if (speed > speedLimit) {
+//     const points = Math.floor((speed-speedLimit)/kmPerPoint);
+//     if (points >= 1) return (points >= 12) ? 'Suspended': `Points: ${points}`;
+//   }
+//   return 'Ok';
+// }
 
-console.log(checkspeed(71));
+// console.log(checkspeed(71));
 
