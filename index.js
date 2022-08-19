3e948cca5a1193723b8ea5103a54e8338989d98a
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-19 12:43:21 +0100

    calculate grade exercise

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
 
+// const calculateGrade = (marks) => {
+//   const average = calculateAverage(marks);
+
+//   if (average < 60) return 'F';
+//   if (average < 70) return 'D';
+//   if (average < 80) return 'C';
+//   if (average < 90) return 'B';
+//   return 'A'
+// }
+
+// const calculateAverage = (array) => {
+//   let sum = 0;
+//   for (value of array)
+//     sum += value;
+//   return sum / array.length;
+// }
+
+// const marks = [90, 70, 90];
+
+// console.log(calculateGrade(marks));
+
 // const sum = (limit) => {
 //   let answer = 0;
 
