cddd94f547daaf09a1da6134afabd4b0a3eaf6b8
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-08-21 22:47:34 +0100

    constructor functions excercise

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-let blog = {
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false
+}
+
+let post = {
   title: 'a',
   body: 'b',
   author: 'c',
